f6f113d2d177e220e331941d845b6d8b0efa34c3
Sekhar Makkella <makkella.sekhar@capgemini.com>
2022-05-25 14:40:49 +0530

    up2

--- hellooo/src/hellooo/Welcoming.java
+++ hellooo/src/hellooo/Welcoming.java
@@ -6,7 +6,7 @@ public class Welcoming {
 //	welcome team---3 
 //	this is trilok
 		System.out.println("I am Shekhar");
-//ert
+//ertuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu
 	}
 
 }
